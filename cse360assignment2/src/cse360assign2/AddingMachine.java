476bb4054614a82af15b1347d638f876faa8ed5f
ajaytiwari99 <32396282+ajaytiwari99@users.noreply.github.com>
2019-10-08 20:17:57 -0700

    Initial commit of project

---
+++ cse360assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,40 @@
+package cse360assign2;
+
+/**
+* The AddingMachine program implements an application that
+* simply add and subtract "value" that its given. 
+* Also return string and total value. 
+*
+* @author Ajay Tiwari
+* @classID	85141
+* @version Assignment 2
+*
+*/
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - include for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
+	
+	public void clear() {
+		
+	}
+}
